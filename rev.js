9c6d1696028affb481127bab4e12c621a3d7c8dc
Li Han <66966137+kLiHz@users.noreply.github.com>
2023-11-03 20:34:59 +0000

    Flating "else" branches

--- rev.js
+++ rev.js
@@ -23,30 +23,32 @@ async function handleRequest(request, { MAPPER }) {
 
   if (q === '') { // home page
     return new Response("Nope.", { status: 400 });
-  } else if (q === 'favicon.ico') {
-    return fetch('https://workers.cloudflare.com/favicon.ico');
-  } else {
-    let val = await MAPPER.get(q);
-    if (val === null) {  // q not found
-      return new Response("None.", { status: 404 });
-    } else {
-      if (q.startsWith('html:')) {
-        return new Response(val, {
-          headers: {
-            "content-type": "text/html;charset=utf-8"
-          }
-        });
-      } else if (isValidHttpUrl(val)) {
-        const url = new URL(val);
-        if (url.hostname === 'api.github.com') {
-          const headers = new Headers();
-          headers.append('Authorization', `Bearer ${url.password}`);
-          return fetch(url, {headers});
-        }
-        return fetch(val);
-      } else {
-        return new Response("Error.", { status: 500 });
-      }
-    }
   }
+  if (q === 'favicon.ico') {
+    return fetch('https://workers.cloudflare.com/favicon.ico');
+  }
+  const val = await MAPPER.get(q);
+  if (val === null) {  // q not found
+    return new Response("None.", { status: 404 });
+  } 
+  if (q.startsWith('html:')) {
+    return new Response(val, {
+      headers: {
+        "content-type": "text/html;charset=utf-8",
+      },
+    });
+  }
+  if (isValidHttpUrl(val)) {
+    const url = new URL(val);
+    const headers = new Headers();
+    switch (url.hostname) {
+      case 'api.github.com':
+        headers.append('Authorization', `Bearer ${url.password}`);
+        break;
+      default:
+        break;
+    }
+    return fetch(url, {headers});
+  }
+  return new Response("Error.", { status: 500 });
 }
